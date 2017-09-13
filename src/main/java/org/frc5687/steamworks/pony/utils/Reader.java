db2bf555b50a99d0dc5f2edb6d9832c01bdfc862
BenBernardCIS <benbernard@cliffislandsoftware.com>
2017-09-13 11:17:07 -0400

    Iniital clone of Tony's code for Pony.

---
+++ src/main/java/org/frc5687/steamworks/pony/utils/Reader.java
@@ -0,0 +1,10 @@
+package org.frc5687.steamworks.protobot.utils;
+
+/**
+ * A file reader class to read current Git information from deployed robot code
+ * @author wilstenholme
+ */
+
+public class Reader {
+    public static final String gitInfo = "Pony 1f410f4 *";
+ }
